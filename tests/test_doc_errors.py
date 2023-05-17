93fecb91e8b576d29bfd7c698b84cff53f36dd2a
joamatab <4514346+joamatab@users.noreply.github.com>
2023-05-17 10:04:59 -0700

    better fix errors

--- tests/test_doc_errors.py
+++ tests/test_doc_errors.py
@@ -1,5 +1,11 @@
 if __name__ == "__main__":
     from demo_project.config import PATH
 
-    dirpath = PATH.repo / "docs" / "build" / "html" / "reports"
-    assert not dirpath.glob("*err.log")
+    dirpath = PATH.repo / "docs" / "_build" / "html" / "reports"
+    error_files = list(dirpath.glob("*err.log"))
+
+    if error_files:
+        for error_file in error_files:
+            error = error_file.read_text()
+            print(error)
+        raise ValueError(f"{len(error_files)} errors in notebooks")
